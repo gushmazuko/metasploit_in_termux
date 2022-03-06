72932c22953d857b36837e7115cb3eed1b735388
dedshit <62318734+dedshit@users.noreply.github.com>
2022-03-06 17:50:48 +0530

    Fixed warning & inconsistent gem version
    enjoy!

--- metasploit.sh
+++ metasploit.sh
@@ -110,5 +110,15 @@ termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/*/gems/pg-*/lib
 
 echo
 center "*"
+echo -e "\033[32m Suppressing Warnings\033[0m"
+
+sed -i '355 s/::Exception, //' $PREFIX/bin/msfvenom
+sed -i '481, 483 {s/^/#/}' $PREFIX/bin/msfvenom
+sed -Ei "s/(\^\\\c\s+)/(\^\\\C-\\\s)/" /data/data/com.termux/files/home/metasploit-framework/lib/msf/core/exploit/remote/vim_soap.rb
+sed -i '86 {s/^/#/};96 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
+sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/logging-2.3.0/lib/logging/diagnostic_context.rb
+
+echo
+center "*"
 echo -e "\033[32m Installation complete. \n Launch metasploit by executing: msfconsole\033[0m"
 center "*"
